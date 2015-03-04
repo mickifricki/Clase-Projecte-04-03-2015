f67804b17ef0739ed91ce52daf32c068c66661d0
Miquel Sans Canudas <mickie.snas.11@gmail.com>
2015-03-04 12:14:43 +0100

    Joc

---
+++ Joc/Joc/Main.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "SDL2-2.0.3\include\SDL.h"
+
+#pragma comment (lib, "SDL2-2.0.3/lib/x86/SDL2.lib") //o #pragma comment (lib, "SDL2-2.0.3\\lib\\x86\\SDL2.lib") millor el primer, amb la barra cap a la dreta
+#pragma comment (lib, "SDL2-2.0.3/lib/x86/SDL2main.lib")
+int main(int argc, char** argv)
+{
+	return 0;
+}
